c34f4ded4b6314f1728d34e6130953ed58c79d7a
Artem <mora4evskiy@yandex.ru>
2024-07-28 16:03:07 +0300

    I add better start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
 # Путь к виртуальному окружению
-VENV_PATH="path/to/your/venv"
+
+VENV_PATH="$HOME/Desktop/modules/venv"
+
+# Создание необходимых директорий, если их нет
+
+if [ ! -d "$HOME/Desktop/modules" ]; then  
+  echo "Создаем директории $HOME/Desktop/modules"
+  mkdir -p "$HOME/Desktop/modules"
+fi
 
 # Проверка, существует ли виртуальное окружение
 if [ -d "$VENV_PATH" ]; then  
   echo "Виртуальное окружение найдено. Активируем его."
   source "$VENV_PATH/bin/activate"
 else
-  echo "Виртуальное окружение не найдено."  exit 1
+  echo "Виртуальное окружение не найдено. Создаем новое."  
+  python3 -m venv "$VENV_PATH"
+  source "$VENV_PATH/bin/activate"  
+  echo "Устанавливаем необходимые библиотеки..."
+  pip3 install RPi.GPIO opencv-python Adafruit_DHT
 fi
-
+  
 # Установка Geany
 echo "Устанавливаем Geany..."
 sudo apt-get update
 sudo apt-get install -y geany
 
-# Запуск Geany
-echo "Запускаем Geany..."
+# Запуск Geany с активацией виртуального окруженияecho "Запускаем Geany..."
 geany &
+
