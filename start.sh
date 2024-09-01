5f66c308f2c03bcb4ad9ddec2b42f3ca0a9d4bfb
Artem <mora4evskiy@yandex.ru>
2024-09-01 09:25:36 +0100

    update: start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 # Путь к виртуальному окружению
-
 VENV_PATH="$HOME/Desktop/modules/venv"
+REPO_URL="https://github.com/ArtemUnreal/Stek_Pro.git"
+CLONE_DIR="$HOME/Desktop/modules/clone"
 
 # Создание необходимых директорий, если их нет
-
 if [ ! -d "$HOME/Desktop/modules" ]; then  
   echo "Создаем директории $HOME/Desktop/modules"
   mkdir -p "$HOME/Desktop/modules"
 fi
+
+# Клонирование репозитория
+if [ ! -d "$CLONE_DIR" ]; then
+  echo "Клонируем репозиторий $REPO_URL в $CLONE_DIR"
+  git clone "$REPO_URL" "$CLONE_DIR"
+else
+  echo "Репозиторий уже клонирован."
+fi
+
+# Копирование файлов и директорий в /Desktop/modules
+echo "Копируем файлы и директории в $HOME/Desktop/modules"
+cp -r "$CLONE_DIR"/* "$HOME/Desktop/modules/"
 
 # Проверка, существует ли виртуальное окружение
 if [ -d "$VENV_PATH" ]; then  
@@ -20,7 +32,7 @@ else
   python3 -m venv "$VENV_PATH"
   source "$VENV_PATH/bin/activate"  
   echo "Устанавливаем необходимые библиотеки..."
-  pip3 install RPi.GPIO opencv-python Adafruit_DHT
+  pip3 install RPi.GPIO opencv-python Adafruit_DHT paho-mqtt
 fi
   
 # Установка Geany
@@ -28,6 +40,6 @@ echo "Устанавливаем Geany..."
 sudo apt-get update
 sudo apt-get install -y geany
 
-# Запуск Geany с активацией виртуального окруженияecho "Запускаем Geany..."
+# Запуск Geany с активацией виртуального окружения
+echo "Запускаем Geany..."
 geany &
-
